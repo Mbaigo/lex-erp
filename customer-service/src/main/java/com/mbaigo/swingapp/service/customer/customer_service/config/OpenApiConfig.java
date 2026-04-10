746c8bc26e038f3c86cc8f8f8e92ab75dbeeb518
Mbaigo <mbaigolmemevrard@gmail.com>
2026-04-10 10:41:50 +0400

    feature-order-service-epic-5-: update OpenApiConfig

--- customer-service/src/main/java/com/mbaigo/swingapp/service/customer/customer_service/config/OpenApiConfig.java
+++ customer-service/src/main/java/com/mbaigo/swingapp/service/customer/customer_service/config/OpenApiConfig.java
@@ -2,6 +2,7 @@ package com.mbaigo.swingapp.service.customer.customer_service.config;
 
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
@@ -15,7 +16,12 @@ public class OpenApiConfig {
     public OpenAPI customOpenAPI() {
         final String securitySchemeName = "bearerAuth";
         return new OpenAPI()
-                .info(new Info().title("Customer Service API").version("1.0"))
+                .info(new Info().title("API Customer Service - Atelier Couture ERP")
+                        .description("Documentation des API REST pour Gérer l'identité, le contact et l'évolution morphologique (JSONB) des clients.")
+                        .version("1.0.0")
+                        .contact(new Contact()
+                                .name("Atelier Couture")
+                                .email("admin@atelier-couture.com")))
                 .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
                 .components(
                         new Components()
